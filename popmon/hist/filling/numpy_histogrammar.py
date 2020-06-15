9d0173a3ded92108f5f2fcac6c58a84882da04d0
Simon Brugman <sbrugman@users.noreply.github.com>
2020-06-15 20:37:46 +0200

    Add licence headers, make.bat and CI workflow (#29)

--- popmon/hist/filling/numpy_histogrammar.py
+++ popmon/hist/filling/numpy_histogrammar.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+# Copyright (c) 2020 ING Wholesale Banking Advanced Analytics
+# This file is part of the Population Shift Monitoring package (popmon)
+# Licensed under the MIT License
+
 # -*- coding: utf-8 -*-
 
 import numpy as np
