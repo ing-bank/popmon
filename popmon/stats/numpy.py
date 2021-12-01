c6a1ca74da3a4de1f69b919cce047cc0b6377589
Simon Brugman <sfbbrugman@gmail.com>
2021-12-01 16:24:19 +0100

    feat(comparisons): introduce psi and jsd

--- popmon/stats/numpy.py
+++ popmon/stats/numpy.py
@@ -365,6 +365,27 @@ def googl_test(bins_1, bins_2):
     return np.max(np.abs(dist(bins_1) - dist(bins_2)))
 
 
+@Comparisons.register(key="psi", description="Population Stability Index")
+def population_stability_index(p, q):
+    epsilon = 10e-6
+    p += epsilon
+    q += epsilon
+    return np.sum((p - q) * np.log(p / q))
+
+
+def kullback_leibler_divergence(p, q):
+    epsilon = 10e-6
+    p += epsilon
+    q += epsilon
+    return np.sum(p * np.log(p / q))
+
+
+@Comparisons.register(key="jsd", description="Jensen-Shannon Divergence")
+def jensen_shannon_divergence(p, q):
+    m = 0.5 * (p + q)
+    return 0.5 * (kullback_leibler_divergence(p, m) + kullback_leibler_divergence(q, m))
+
+
 def probability_distribution_mean_covariance(entries_list):
     """Mean normalized histogram and covariance of list of input histograms
 
