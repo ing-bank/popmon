00ecd374372a038eb14df25137b6e09ab110fabe
Simon Brugman <sfbbrugman@gmail.com>
2022-05-27 20:08:07 +0200

    docs(synthetic): hyperplane logistic regression example

--- examples/synthetic_data_streams/hyperplane.py
+++ examples/synthetic_data_streams/hyperplane.py
@@ -1,6 +1,7 @@
 """
 Example configuration for the hyperplane dataset
 """
+from sklearn.linear_model import LogisticRegression
 from synthetic_data_streams import (
     dataset_summary,
     load_arff,
@@ -13,12 +14,25 @@ v = "1"
 # Monitor the each feature w.r.t. the label
 features = [f"index:attr{i}:output" for i in range(10)]
 
+# Also monitor predictions w.r.t. the label (see below)
+features += ["index:prediction:output"]
+
 dataset_file = f"data/{dataset_name}{v}.arff"
 report_file = f"reports/{dataset_name}_{v}.html"
 
 df = load_arff(dataset_file)
 
+# Fit a logistic regression on the first 10% of the data.
+model = LogisticRegression(C=1e5)
+model.fit(df.loc[:1000, df.columns != "output"], df.loc[:1000, "output"])
+
+# Use the model to predict over the full dataset
+df["prediction"] = model.predict_proba(df.loc[:, df.columns != "output"])[:, 1]
+
 dataset_summary(df)
 
-# Reduce the time_width for this smaller dataset
-synthetic_data_stream_report(df, features, report_file, time_width=500)
+# The training set for the model will be used as reference.
+# The reduced time_width is because this is a smaller dataset compared to the rest
+synthetic_data_stream_report(
+    df, features, report_file, time_width=500, reference="start", split=1000
+)
