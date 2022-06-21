d46e805ccabfb8639b3ab13151a92887f69bb691
Simon Brugman <sfbbrugman@gmail.com>
2022-06-21 14:02:48 +0200

    chore: update example syntax

--- examples/flight_delays.py
+++ examples/flight_delays.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
 import popmon
-from popmon import resources
+from popmon import Settings, resources
 
 # open synthetic data
 df = pd.read_csv(
     resources.data("flight_delays.csv.gz"), index_col=0, parse_dates=["DATE"]
 )
+
+
+# Configuration of the monitoring rules and report
+settings = Settings()
+settings.report.extended_report = False
+settings.monitoring.pull_rules = {"*_pull": [10, 7, -7, -10]}
 
 # generate stability report using automatic binning of all encountered features
 # (importing popmon automatically adds this functionality to a dataframe)
@@ -16,8 +22,7 @@ report = popmon.df_stability_report(
     time_axis="DATE",
     time_width="1w",
     time_offset="2015-07-02",
-    extended_report=False,
-    pull_rules={"*_pull": [10, 7, -7, -10]},
+    settings=settings,
 )
 
 # or save the report to file
