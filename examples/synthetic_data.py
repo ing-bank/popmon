6f94c9fc3c0911d255d18eb906ca580335fb6ddf
Simon Brugman <sfbbrugman@gmail.com>
2022-06-28 17:51:36 +0200

    fix: set time_width in synthetic data example

--- examples/synthetic_data.py
+++ examples/synthetic_data.py
@@ -8,7 +8,11 @@ df = pd.read_csv(resources.data("test.csv.gz"), parse_dates=["date"])
 
 # generate stability report using automatic binning of all encountered features
 # (importing popmon automatically adds this functionality to a dataframe)
-report = df.pm_stability_report(time_axis="date", features=["date:age", "date:gender"])
+report = df.pm_stability_report(
+    time_axis="date",
+    time_width="2w",
+    features=["date:age", "date:gender", "date:isActive", "date:eyeColor"],
+)
 
 # or save the report to file
 report.to_file("test_data_report.html")
